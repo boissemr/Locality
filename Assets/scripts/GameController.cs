76e01f9c2eddd45aea236b2bf72f85a7b135d1a5
Matt Boisseau <boissemr@miamioh.edu>
2016-04-11 01:40:00 -0400

    transition to gameplay
    tweaked title screen and added working transition to gameplay

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject	tilePrefab;
+
+	public void expand() {
+		GameObject child = (GameObject)Instantiate(tilePrefab, transform.position, Quaternion.identity);
+		child.transform.parent = transform;
+	}
+}
--- Assets/scripts/MenuSystem.cs
+++ Assets/scripts/MenuSystem.cs
@@ -6,8 +6,14 @@ public class MenuSystem : MonoBehaviour {
 	public MenuObject	title,
 						mainMenu,
 						tapToBeginText,
-						titleBackdrop;
-	public float		menuTransitionDuration;
+						titleBackdrop,
+						titleBackgroundScene,
+						gameplayArea;
+	public GameObject	titleCamera,
+						gameplayCamera;
+	public GameController gameController;
+	public float		menuTransitionDuration,
+						fadeToGameplayDuration;
 
 	// called when screen tapped on in title screen
 	public void tapToBegin() {
@@ -22,5 +28,19 @@ public class MenuSystem : MonoBehaviour {
 		titleBackdrop.fadeTo(0, menuTransitionDuration);
 		StartCoroutine(title.moveTo(Camera.main.ViewportToScreenPoint(new Vector3(-1f, 0.5f, 0)), menuTransitionDuration));
 		StartCoroutine(mainMenu.moveTo(Camera.main.ViewportToScreenPoint(new Vector3(2f, 0.5f, 0)), menuTransitionDuration));
+		StartCoroutine(titleBackgroundScene.moveTo(new Vector3(0, -100, 0), fadeToGameplayDuration));
+		StartCoroutine(instantiatePlayableArea(fadeToGameplayDuration));
+	}
+
+	IEnumerator instantiatePlayableArea(float secondsToWait) {
+		
+		StartCoroutine(gameplayArea.moveTo(new Vector3(0, 0, 0), secondsToWait));
+		
+		yield return new WaitForSeconds(secondsToWait);
+
+		titleBackgroundScene.gameObject.SetActive(false);
+		gameController.expand();
+		titleCamera.SetActive(false);
+		gameplayCamera.SetActive(true);
 	}
 }
