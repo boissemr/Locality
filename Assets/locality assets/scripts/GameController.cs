3acd2281d9fff2181e78b431a8e108429e021d62
Matt Boisseau <boissemr@miamioh.edu>
2016-04-12 16:15:08 -0400

    removed ambient occlusion
    because it was causing dramatic framerate issues

--- Assets/locality assets/scripts/GameController.cs
+++ Assets/locality assets/scripts/GameController.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 
 	public GameObject	tilePrefab;
+	public Light		sunlight;
 
 	public void expand() {
 		GameObject child = (GameObject)Instantiate(tilePrefab, transform.position, Quaternion.identity);
 		child.transform.parent = transform;
 	}
+
+	/*
+	public IEnumerator setSunlight(float value, float duration) {
+		float	timer = duration,
+				currentValue = sunlight.
+		timer -= Time.deltaTime;
+		sunlight.intensity = value;
+	}
+	*/
 }
--- Assets/locality assets/scripts/MenuSystem.cs
+++ Assets/locality assets/scripts/MenuSystem.cs
@@ -37,6 +37,7 @@ public class MenuSystem : MonoBehaviour {
 
 		gameController.expand();
 		StartCoroutine(gameplayArea.moveTo(new Vector3(0, 0, 0), secondsToWait));
+		//StartCoroutine(gameController.setSunlight(1f, secondsToWait));
 		
 		yield return new WaitForSeconds(secondsToWait);
 
